33dd0016bd31e045d1cd7805d85410ed9260c4ea
nishashaik31 <135350885+nishashaik31@users.noreply.github.com>
2023-12-19 12:59:47 +0530

    created Sort.java

--- src/main/java/com/abc/binary/Sort.java
+++ src/main/java/com/abc/binary/Sort.java
@@ -4,7 +4,7 @@ public static int[] sorting(int[] a){
 int temp=0
 for(int i=0;i<a.length;i++){
 for(int j=i+1;j<a.length;j++){
-if(a[i][j[C<a[j]){
+if(a[i]<a[j]){
 temp=a[i];
 a[i]=a[j];
 a[j]=temp;
