97b9c12b879e6a071f9b784fa2a975a2dc2211e8
dinisha <dinishashaik2003@gmail.com>
2023-12-19 12:51:53 +0530

    sort file created

---
+++ src/main/java/com/abc/binary/Sort.java
@@ -0,0 +1,16 @@
+package com.abc.binary;
+public class Sort{
+public static int[] sorting(int[] a){
+int temp=0
+for(int i=0;i<a.length;i++){
+for(int j=i+1;j<a.length;j++){
+if(a[i][j[C<a[j]){
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+return a;
+}
+}
