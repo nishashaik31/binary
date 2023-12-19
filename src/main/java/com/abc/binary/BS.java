ec63df19dca1c811b6dcce23ac77698cc1eb763b
nishashaik31 <135350885+nishashaik31@users.noreply.github.com>
2023-12-19 13:36:09 +0530

    BS file created in linux

--- src/main/java/com/abc/binary/BS.java
+++ src/main/java/com/abc/binary/BS.java
@@ -1,12 +1,12 @@
 package com.abc.binary;
 publi class BS{
 public static void main(string[] arg){
-int[] a=[4,5,6,7];
+int a[]=[4,5,6,7];
 int first=0;
 int last=a.length-1;
 int mid=(first+last)/2;
 int key=5;
-int[] a=Sort.sorting(a);
+int a[]=Sort.sorting(a);
 while(first<=last){
 if(a[mid]<key){
 first=mid+1;
